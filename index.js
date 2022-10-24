0a28a7f0b277f700e2edef66687883ffeb9daeda
Olga Repina <repinaov98@gmail.com>
2022-10-24 18:54:33 +0400

    changes popup

---
+++ index.js
@@ -0,0 +1,97 @@
+async function start() {
+    let usersCardBox = document.querySelector('#cardBox');
+    let usersPopup = document.querySelector('#userPopup');
+    const input = document.querySelector('#input')
+    let users = await fetchUsers();
+    let filterUsers = fetchFilterUsers();
+
+
+    input.oninput = function () {
+        let value = this.value.trim().toLowerCase();
+        return value
+    }
+
+    if (value !== '') {
+        usersCardBox.innerHTML = getUserCards(users).join('');
+        usersPopup.innerHTML = getUserModals(users).join('');
+    } else {
+
+        usersCardBox.innerHTML = getUserCards(filterUsers).join('');
+        usersPopup.innerHTML = getUserModals(filterUsers).join('');
+    }
+}
+
+
+
+function fetchUsers() {
+    const url = 'http://127.0.0.1:3000'
+
+    return fetch(url).then(res => res.json())
+}
+
+function fetchFilterUsers(value) {
+    const url = `http://127.0.0.1:3000/?term=${value}`
+
+    return fetch(url).then(res => res.json())
+}
+
+function getUserCard(user) {
+    let userName = user.name.replace(/\s+/g, '');
+    return `
+        <div class="card" data-id="${userName}">
+          <h3 class="card--title">${user.name}</h3>
+          <div class="card--phone">
+            <img src="style/icon/phone.svg" width="16" height="16" alt="телефон">
+            <p>${user.phone}</p>
+          </div>
+          <div class="card--mail">
+            <img src="style/icon/envelope.svg" width="16" height="16" alt="почта">
+            <a href="mailto:${user.email}">${user.email}</a>
+          </div>
+        </div>
+        `;
+}
+
+function getUserModal(user) {
+    let userName = user.name.replace(/\s+/g, '');
+    return `
+        
+            <div class="pop-up" data-id="${userName}">
+              <button class="pop-up--close"><img src="style/icon/popup-close.svg" width=""></button>
+              <h3 class="pop-up--title">${user.name}</h3>
+              <div class="pop-up--phone pop-up--flex">
+                <p class="pop-up--subtitle">Телефон:</p>
+                <a class="color-gray" href="tel:${user.phone}">${user.phone}</a>
+              </div>
+              <div class="pop-up--mail pop-up--flex">
+                <p class="pop-up--subtitle">Почта:</p>
+                <a class="color-gray" href="mailto:${user.email}">${user.email}</a>
+              </div>
+              <div class="pop-up--date pop-up--flex">
+                <p class="pop-up--subtitle">Дата приема:</p>
+                <p class="color-gray">${user.hire_date}</p>
+              </div>
+              <div class="pop-up--position pop-up--flex">
+                <p class="pop-up--subtitle">Должность:</p>
+                <p class="color-gray text-overflow">${user.position_name}</p>
+              </div>
+              <div class="pop-up--subdivision pop-up--flex">
+                <p class="pop-up--subtitle">Подразделение:</p>
+                <p class="color-gray text-overflow">${user.department}</p>
+              </div>
+              <div class="pop-up--info">
+                <p class="pop-up--subtitle">Дополнительная информация:</p>
+                <p class="color-gray">${user.address}</p>
+              </div>
+            </div>
+        `;
+}
+
+function getUserCards(users) {
+    return  users.map(el => getUserCard(el))
+}
+
+function getUserModals(users) {
+    return users.map(el => getUserModal(el))
+}
+
---
+++ search.js
@@ -0,0 +1,29 @@
+// async
+// const input = document.querySelector('#input')
+//
+// input.oninput = function () {
+//     let value = this.value.trim().toLowerCase();
+//     if (value !== '') {
+//         function fetchUsers() {
+//             const url = `http://127.0.0.1:3000/?term=${value}`
+//
+//             return fetch(url).then(res => res.json())
+//     }
+//
+//     }
+//     // if (value !== '') {
+//     //     cards.forEach(function (card) {
+//     //
+//     //         let title = card.querySelector('.card--title')
+//     //         if (title.innerText.toLowerCase().search(value) === -1) {
+//     //             card.classList.add('hide')
+//     //         } else {
+//     //             card.classList.remove('hide')
+//     //         }
+//     //     })
+//     // } else {
+//     //     cards.forEach(card => {
+//     //         card.classList.remove('hide')
+//     //     })
+//     // }
+// }
